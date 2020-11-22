9d28a7fb238db78fc182d2f33828adb76518ba74
Tariqfaqira <55322383+Tariqfaqira@users.noreply.github.com>
2020-11-23 04:29:23 +0500

    5 product model is updated and add product is working

--- models/products.model.js
+++ models/products.model.js
@@ -1,35 +1,114 @@
-const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate');
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
-    name: {
-        type: String
+  user_id: {
+    type: Schema.Types.ObjectId,
+  },
+  building_name: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  flat_per_floor: {
+    type: String,
+  },
+  floors: {
+    type: String,
+  },
+  four_bed_length: {
+    type: String,
+  },
+  four_bed_width: {
+    type: String,
+  },
+  four_bed_price: {
+    type: String,
+  },
+  three_bed_length: {
+    type: String,
+  },
+  three_bed_width: {
+    type: String,
+  },
+  three_bed_price: {
+    type: String,
+  },
+  two_bed_length: {
+    type: String,
+  },
+  two_bed_width: {
+    type: String,
+  },
+  two_bed_price: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  variations: [
+    {
+      floor_no: { type: String },
+      flats: [
+        {
+          bed_type: { type: String },
+          flat_no: { type: String },
+        },
+      ],
     },
-    size: {
-        type: String
-    },
-    quantity: {
-        type: String,
-        unique: true,
-        sparse: true
-    },
-    category: {
-        type: String
-    },
-    address: {
-        type: String
-    },
-    // image: {
-    //     data: Buffer, 
-    //     contentType: String
-    // },
-    is_deleted: {
-        type: Boolean,
-        default: false
-    }
+  ],
+
+  entry_date: {
+    type: Date,
+  },
+
+  isdeleted: {
+    type: Boolean,
+  },
 });
 
 Product.plugin(mongoosePaginate);
 
 module.exports = mongoose.model("Product", Product);
+
+// Before
+// const mongoose = require('mongoose');
+// const mongoosePaginate = require('mongoose-paginate');
+// const Schema = mongoose.Schema;
+
+// const Product = new Schema({
+//     name: {
+//         type: String
+//     },
+//     size: {
+//         type: String
+//     },
+//     quantity: {
+//         type: String,
+//         unique: true,
+//         sparse: true
+//     },
+//     category: {
+//         type: String
+//     },
+//     address: {
+//         type: String
+//     },
+//     // image: {
+//     //     data: Buffer,
+//     //     contentType: String
+//     // },
+//     is_deleted: {
+//         type: Boolean,
+//         default: false
+//     }
+// });
+
+// Product.plugin(mongoosePaginate);
+
+// module.exports = mongoose.model("Product", Product);
