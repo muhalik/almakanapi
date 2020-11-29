72747a4ff073996d77fc5f65e4f011527222f77d
Muhalik <61157050+muhalik@users.noreply.github.com>
2020-11-29 15:27:45 +0300

    Update products.routes.js

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -12,7 +12,7 @@ router.get("/product-by-id/:_id", productController.getSingleProduct);
 // router.get("/any/product-by-id/:_id", productController.get_all_product_by_id);
 
 // router.get("/abc/cde/vendor/user-products/:_id", productController.get_vendor_products);
-// router.get("/user-products-query-search/:_id", productController.get_vendor_product_query_search);
+router.get("/user-products-query-search/:_id", productController.get_vendor_product_query_search);
 
 // router.get("/products-all-count/:_id", productController.geteverything);
 // router.get("/search/abc", productController.get_search_products);
@@ -21,7 +21,13 @@ router.get("/product-by-id/:_id", productController.getSingleProduct);
 // router.get("/vendor-search-less-stock/:_id", productController.get_vendor_product_less_stock_by_id);
 
 router.post("/add", productController.addProduct);
+
+
 router.put("/:_id", checkAuth, productController.updateProduct);
+router.put("/review", checkAuth, productController.add_review);
+
+
+
 router.delete("/:_id", checkAuth, productController.deleteProduct);
 
 module.exports = router;
